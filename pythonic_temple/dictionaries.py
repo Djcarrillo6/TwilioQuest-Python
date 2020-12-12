8d788e258be6361c67157afe6033d9895cc0ea0f
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-12-11 16:07:54 -0800

    mods

--- pythonic_temple/dictionaries.py
+++ pythonic_temple/dictionaries.py
@@ -2,7 +2,7 @@ super_hero = {
     "name": 'Miles Morales',
     "hero_name": 'Spider-Man',
     "power_level": 9999,
-    "shield_level": 154
+    "shield_level": 1542
 }
 
 # print(super_hero['name'])
