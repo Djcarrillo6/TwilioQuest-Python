02081cbf2d2522776a3f98da4f1103a65bf6f7b7
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-12-13 09:48:39 -0800

    update templt props

--- pythonic_temple/dictionaries.py
+++ pythonic_temple/dictionaries.py
@@ -2,7 +2,7 @@ super_hero = {
     "name": 'Miles Morales',
     "hero_name": 'Spider-Man',
     "power_level": 9999,
-    "shield_level": 1543
+    "shield_level": 154388
 }
 
 # print(super_hero['name'])
