601a2919954b973517d6a44429953c27a3ed0882
David Carrillo Jr <djcarrillo6@yahoo.com>
2021-02-13 21:53:23 -0800

    New mods

--- branching.py
+++ branching.py
@@ -9,7 +9,7 @@ def add_numbers(first_number, second_number):
     resulting_sum = first_number + second_number
 
     if resulting_sum <= 0:
-        print("You have chosen the path of destitution.")
+        print("You have chosen the Path of destitution.")
     elif resulting_sum >= 1 and resulting_sum <= 100:
         print("You have chosen the path of plenty.")
     elif resulting_sum > 100:
